e932559d0fc300cb0f8f34b17e51029eb027fe05
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2024-06-07 12:41:24 -0300

    clase de hoy

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from 'express'
+import express from 'express';
 const app = express();
 
-
-app.get('/',(req, res)=>{
-  res.send = (" Hello World");
+app.get('/', (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(3000, () =>{;
-  console.log("Example applistening on por 3000");
+app.get('/saludo/valentin', (req, res) => {
+  res.send("Hola Valentin");
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000");
 });
