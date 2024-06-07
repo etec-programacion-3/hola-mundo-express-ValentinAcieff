180a2378e7947506aa4f5bb32cd9cf96f37fb5e5
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2024-06-07 12:19:33 -0300

    cambios

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express  = require('express');
+import express from 'express'
 const app = express();
-const port = 3000 ;
 
-app.get('/:nombre',(req, res)=>{
-  const nombre = req.params.nombre;
-  res.send('Hola ${nombre}');
+
+app.get('/',(req, res)=>{
+  res.send = (" Hello World");
 });
 
-app.listen(port, () =>{
-  console.log('El server se encuentra en http://localhost:${port}');
+app.listen(3000, () =>{;
+  console.log("Example applistening on por 3000");
 });
